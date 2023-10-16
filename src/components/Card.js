0751bc6f8e0283d61a4a379f8dd392bb50d7a2c7
panupatspyksw <pnpspyksw@gmail.com>
2023-10-16 10:58:18 +0700

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from 'react'
 
 const Card = ({ name, email, id }) => {
-  return (
-    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
-      <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    </div>
-  );
+    let normal = `https://robohash.org/${id}?size=300x300`
+    let small = `https://robohash.org/${id}?size=300x300`
+    return (
+        <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+            <img
+                alt='robots'
+                width='200px'
+                height='200px'
+                srcSet={`${normal} 480w, ${small} 1080w`}
+                src={normal}
+            />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    )
 }
 
-export default Card;
+export default Card
