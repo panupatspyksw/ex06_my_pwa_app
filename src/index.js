d28ee8296c5dab3b2d27bb5f9435e9db9caacc8e
panupatspyksw <pnpspyksw@gmail.com>
2023-10-16 10:13:08 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
-import { createRoot } from "react-dom/client";
-import './index.css';
-import App from './containers/App';
-import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './containers/App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
 
+import { Provider } from 'react-redux'
+import 'tachyons'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { requestRobots, searchRobots } from './reducers'
+import { createLogger } from 'redux-logger'
+import ThunkMiddleware from 'redux-thunk'
+const logger = createLogger()
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(ThunkMiddleware, logger))
 
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
